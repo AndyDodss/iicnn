9073246e44d879ab34491bf12761b4e937e1948c
Andrew Karam <36473436+AndyDodss@users.noreply.github.com>
2019-09-25 03:10:17 +0200

    Update utils.py

--- icnn/utils.py
+++ icnn/utils.py
@@ -153,7 +153,7 @@ def sort_layer_list(net, layer_list):
     layer_index_list = []
     for layer in layer_list:
         # net.blobs is collections.OrderedDict
-        for layer_index, layer0 in enumerate(net.blobs.keys(),0):
+        for layer_index, layer0 in enumerate(list(net.blobs.keys())):
             if layer0 == layer:
                 layer_index_list.append(layer_index)
                 break
